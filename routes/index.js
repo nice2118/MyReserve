45e94d128f6b8da912ee884b0d5c093277e74dba
= <=>
2022-12-31 21:41:33 +0700

    V2

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
-CheckStatus: Boolean;
+// CheckStatus: Boolean;
+CheckStatus = false;
+UserPK = 0;
+// UserPK: Number = 0;
+// UserName: String = "";
 
 var mysql = require('mysql');
 var db = mysql.createPool({
@@ -19,20 +23,23 @@ router.get('/connect', function(req, res, next) {
     }
 });
 router.get('/', function(req, res, next) {
+    CheckStatus = false;
+    UserPK = 0;
     res.render('Login', { datatabelid: {}, A: '0' });
 });
 router.post('/', function(req, res, next) {
     if (req.body.User != "" && req.body.Password != "") {
         db.query('SELECT * FROM user WHERE User = ? and Password = ?', [req.body.User, req.body.Password], function(err, rs) {
-            // console.log(rs[0].Status);
             if (rs[0] != undefined) {
                 if (rs[0].Status == 'Admin') {
                     CheckStatus = true;
                 } else {
                     CheckStatus = false;
                 }
-                // console.log(CheckStatus);
-                res.render('Reserve', { title: 'Reserve' });
+                UserPK = rs[0].No;
+                db.query('SELECT * FROM seat', function(err, rs) {
+                    res.render('Reserve', { datatabelid: rs, C: CheckStatus });
+                })
             } else {
                 res.render('Login', { datatabelid: {}, A: '1' });
             }
@@ -54,6 +61,29 @@ router.post('/Register', function(req, res, next) {
     }
 });
 router.get('/Reserve', function(req, res, next) {
-    res.render('Reserve', { title: 'Reserve' });
+    db.query('SELECT * FROM seat', function(err, rs) {
+        res.render('Reserve', { datatabelid: rs, C: CheckStatus });
+    });
 });
+router.get('/ShowData', function(req, res, next) {
+    db.query('SELECT * FROM `reserve` INNER JOIN `seat` ON `reserve`.`SeatNo` = `seat`.`No` INNER JOIN `user` ON `reserve`.`UserNo` = `user`.`No`', function(err, rs) {
+        res.render('ShowData', { datatabelid: rs, C: CheckStatus });
+    });
+
+});
+router.get('/Send', function(req, res, next) {
+    db.query('SELECT * FROM `reserve` WHERE UserNo = ?', UserPK, function(err, rs) {
+        console.log(req.query.id);
+        if (rs[0] == undefined) {
+            db.query("INSERT INTO `reserve` (`No`, `UserNo`, `SeatNo`) VALUES (NULL, ?, ?);", [UserPK, req.query.id], function(err, rs) {
+                res.render('Login', { datatabelid: {}, A: '0' });
+            });
+            db.query("UPDATE `seat` SET `Status` = 'Reserve' WHERE `seat`.`No` = ?;", req.query.id, function(err, rs) {
+                res.render('Login', { datatabelid: {}, A: '0' });
+            });
+        }
+        res.redirect('/Reserve');
+    });
+});
+
 module.exports = router;
