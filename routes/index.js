f6ebb5256d86fc53694ceca1aac19f28a8b81eb4
= <=>
2022-12-31 13:04:43 +0700

    V1

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+CheckStatus: Boolean;
 
 var mysql = require('mysql');
 var db = mysql.createPool({
@@ -10,7 +11,6 @@ var db = mysql.createPool({
     debug: false
 });
 
-/* GET Check Status MySQL. */
 router.get('/connect', function(req, res, next) {
     if (db != null) {
         res.send('Successful');
@@ -18,10 +18,42 @@ router.get('/connect', function(req, res, next) {
         res.send('Unsuccessful');
     }
 });
-
-/* GET Login */
 router.get('/', function(req, res, next) {
-    res.render('Login', { title: 'Login' });
+    res.render('Login', { datatabelid: {}, A: '0' });
 });
-
+router.post('/', function(req, res, next) {
+    if (req.body.User != "" && req.body.Password != "") {
+        db.query('SELECT * FROM user WHERE User = ? and Password = ?', [req.body.User, req.body.Password], function(err, rs) {
+            // console.log(rs[0].Status);
+            if (rs[0] != undefined) {
+                if (rs[0].Status == 'Admin') {
+                    CheckStatus = true;
+                } else {
+                    CheckStatus = false;
+                }
+                // console.log(CheckStatus);
+                res.render('Reserve', { title: 'Reserve' });
+            } else {
+                res.render('Login', { datatabelid: {}, A: '1' });
+            }
+        });
+    } else {
+        res.render('Login', { datatabelid: {}, A: '2' });
+    }
+});
+router.get('/Register', function(req, res, next) {
+    res.render('Register', { datatabelid: {}, A: '0' });
+});
+router.post('/Register', function(req, res, next) {
+    if (req.body.User != "" && req.body.Password != "") {
+        db.query("INSERT INTO user SET ?", req.body, function(err, rs) {
+            res.render('Login', { datatabelid: {}, A: '0' });
+        });
+    } else {
+        res.render('Register', { datatabelid: {}, A: '1' });
+    }
+});
+router.get('/Reserve', function(req, res, next) {
+    res.render('Reserve', { title: 'Reserve' });
+});
 module.exports = router;
